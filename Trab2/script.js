6c2f61a2cf53fb9882d590801703cde3d738c7ed
Lucas <luukascastro@gmail.com>
2024-10-28 08:19:43 -0300

    fix: Arquivos corrigidos para ultima versao

--- Trab2/script.js
+++ Trab2/script.js
@@ -1,59 +1,64 @@
 const senha = "moutinho"
-let selecionado = 0;
+const taskContainer = document.querySelector("#tasks")
+const taskCounter = document.querySelector("#taskCounter")
+const removeBtn = document.querySelector("#removeTask")
+const tasks = []
 
-function validarFormulario()
-{
-    validarNomeTarefa()
-    validarDescricao()
-    validarData()
-    validarPrioridade()
-    validarSenha()
+
+function renderTasks() {
+  let concat = ""
+  tasks.forEach(o => concat += `<p> ${o} </p>`)
+
+  taskContainer.innerHTML = concat
+  taskCounter.innerHTML = `Quantidade de tarefas: ${tasks.length}`
 }
 
-function validarNomeTarefa()
-{
-    let nomeTarefa = document.getElementById("txtNomeTarefa").value
-    if (nomeTarefa == "" || nomeTarefa.length <= 4)
-    {
-        alert("Nome da tarefa está vazia ou possui menos de 5 letras!")
-        return false
-    }
+function removeTask() {
+  tasks.pop()
+  renderTasks()
 }
 
-function validarDescricao()
-{
-    if (document.getElementById("txtDescricao").value == "")
-    {
-        alert("Descrição não pode ser vazia!")
-        return false
-    }
-}
+removeBtn.addEventListener('click', removeTask)
 
-function validarData()
-{
-    if (document.getElementById("dateDataVencimento").value == "")
-    {
-        alert("Data não pode ser vazia!")
-        return false
-    }
-}
+renderTasks()
 
-function validarPrioridade()
-{
-    //Com certeza tem uma maneira mais fácil, mas tamo sem tempo.(isso foi digitado as 09:45)
-    if (selecionado == 0)
-    {
-        alert("Prioridade não selecionada!")
-        return false
+function validarFormulario() {
+  let nomeTarefa = document.getElementById("txtNomeTarefa").value
+  if (nomeTarefa == "" || nomeTarefa.length <= 4) {
+    alert("Nome da tarefa está vazia ou possui menos de 5 letras!")
+    return false
+  }
 
-    }
-}
+  if (document.getElementById("txtDescricao").value == "") {
+    alert("Descrição não pode ser vazia!")
+    return false
+  }
 
-function validarSenha()
-{
-    if (document.getElementById("txtSenha").value != senha)
-    {
-        alert("Senha incorreta!")
-        return false
+
+  if (document.getElementById("dateDataVencimento").value == "") {
+    alert("Data não pode ser vazia!")
+    return false
+  }
+
+
+  let rbCounter = 0
+  let rbList = document.getElementsByName("Prioridade")
+  rbList.forEach(e => {
+    if (e.checked == false) {
+      rbCounter++
     }
+  })
+  if (rbCounter == 3) {
+    alert("Uma prioridade deve ser selecionada!")
+    return false
+  }
+
+  if (document.getElementById("txtSenha").value != senha) {
+    alert("Senha incorreta!")
+    return false
+  }
+
+  tasks.push(nomeTarefa)
+  renderTasks()
+  console.log("Ok");
 }
